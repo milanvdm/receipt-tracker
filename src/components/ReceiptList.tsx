afb97d2ae754ecd65dcc42008177824da2450733
milanvdm <milanvdm@hotmail.com>
2019-04-15 21:43:02 +0900

    Add mobx receiptList to ReceiptList component

--- src/components/ReceiptList.tsx
+++ src/components/ReceiptList.tsx
@@ -1,39 +1,25 @@
-import React, { Component } from 'react';
-import { Box, Text , InfiniteScroll} from 'grommet';
+import React from 'react';
+import { Box , Table, TableBody, TableRow, TableCell } from 'grommet';
+import { observer } from 'mobx-react'
 
-const ITEMS: number[] = [];
-while (ITEMS.length < 100) ITEMS.push(ITEMS.length);
+import Receipt from './Receipt';
+import { ReceiptData } from '../store/Store'
 
-class ReceiptList extends Component {
-    public render(): JSX.Element {
-        return (
-            <Box 
-                direction='column' 
-                margin='small'
-                overflow='scroll'
-                height='large'
-            >
-                <InfiniteScroll 
-                    items={ITEMS} 
-                    step={10} 
-                    replace 
-                    scrollableAncestor='window'
-                    onMore={(): void => console.log('!!! onMore')}
-                >
-                    {(item, index): JSX.Element => (
-                        <Box
-                            key={index}
-                            pad='medium'
-                            background={`neutral-${(index % 4) + 1}`}
-                            align='center'
-                        >
-                            <Text size='large' weight='bold' color='white'>{index}</Text>
-                        </Box>
-                    )}
-                </InfiniteScroll>
-            </Box>
-        );
-    }
-}
+interface ReceiptListProps { receiptList: ReceiptData[] }
 
-export default ReceiptList
+const ReceiptList = ({ receiptList }: ReceiptListProps): JSX.Element =>
+    <Box margin='small' >
+        <Table alignSelf='center'>
+            <TableBody>
+                {receiptList.map((receipt): JSX.Element => (
+                    <TableRow key={receipt.id}>
+                        <TableCell>
+                            <Receipt />
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </Box>
+
+export default observer(ReceiptList)
