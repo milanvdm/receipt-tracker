5e4bbb213abaf225a62998991f01043b2519b86d
milanvdm <milanvdm@hotmail.com>
2019-04-15 21:44:20 +0900

    Add mobx Store to Home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,33 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Box } from 'grommet';
 
 import Total from '../components/Total';
 import ReceiptList from '../components/ReceiptList';
+import AddButton from '../components/AddButton';
+import { Store } from '../store/Store'
 
-class Home extends Component {
-    public render(): JSX.Element {
-        return (
-            <Box background='light-4'>
-                <Box 
-                    border 
-                    round
-                    elevation='small'
-                    background='dark-2' 
-                >
-                    <ReceiptList />
-                    <Box 
-                        border='top'
-                        align='center'
-                        round={{ "size": "small", "corner": "bottom" }}
-                        fill
-                        background='brand'
-                    >
-                        <Total />
-                    </Box>
-                </Box>
+interface HomeProps { store: Store }
+
+const Home = ({ store }: HomeProps): JSX.Element =>
+    <Box 
+        background='light-4'
+        border 
+        round
+        fill
+        elevation='small'
+    >
+        <Box 
+            id="test" 
+            overflow='scroll'
+            fill='vertical'
+        >
+            <ReceiptList receiptList={store.receiptList} />
+        </Box>
+        <Box
+            border='top'
+            align='center'
+            round={{ "size": "small", "corner": "bottom" }}
+            direction='row'
+        >
+            <Box 
+                flex='grow' 
+                align='start'
+                pad={{left: 'medium'}}
+            >
+                <AddButton label='Add Receipt' onClick={(): void => store.addReceipt()} />
             </Box>
-        );
-    }
-}
+            <Box
+                flex='grow'
+                align='end'
+                pad={{right: 'medium', bottom: 'small', top: 'small'}}
+            >
+                <Total value={123} />
+            </Box>
+        </Box>
+    </Box>
 
 export default Home
