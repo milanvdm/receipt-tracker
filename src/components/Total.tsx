aab97556ae0605a870679f6c99fafaf84df1d8c8
milanvdm <milanvdm@hotmail.com>
2019-04-15 17:33:14 +0900

    Update Total to have a dynamic value

--- src/components/Total.tsx
+++ src/components/Total.tsx
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import { Box, Text } from 'grommet';
 import { Currency } from 'grommet-icons';
 
-class Total extends Component {
-    public render(): JSX.Element {
-        return (
-            <Box 
-                direction='row-responsive' 
-                gap='small' 
-                margin='small'
-            >
-                <Currency color='neutral-1' /> 
-                <Text>123</Text>
-            </Box>
-        );
-    }
-}
+interface TotalProps { value: number }
+
+const Total = ({ value }: TotalProps): JSX.Element =>
+    <Box 
+        direction='row' 
+        gap='small' 
+        margin='small'
+        align='center'
+    >
+        <Currency color='neutral-1' size='medium' /> 
+        <Text size='large' weight='bold'>{value}</Text>
+    </Box>
 
 export default Total
