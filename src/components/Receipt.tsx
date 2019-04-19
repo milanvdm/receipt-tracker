89d15da32001b36f414124ecc0be2fd034bb406a
milanvdm <milanvdm@hotmail.com>
2019-04-20 00:13:49 +0900

    Add Receipt component

--- src/components/Receipt.tsx
+++ src/components/Receipt.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { Box } from 'grommet';
+import { observer } from 'mobx-react'
+import { List } from 'immutable';
 
 import Total from './Total';
 import Categories from './Categories';
 import AddButton from './AddButton';
+import { ReceiptId, ExpenseId, ExpenseData } from '../store/Store'
+import ExpenseList from './ExpenseList';
 
-const Receipt = (): JSX.Element => 
+interface ReceiptProps { 
+    id: ReceiptId;
+    category?: string;
+    expenses: List<ExpenseData>;
+    addExpense: (receiptId: ReceiptId) => void;
+    updateCategory: (receiptId: ReceiptId, category: string) => void;
+    updateNote: (receiptId: ReceiptId, expenseId: ExpenseId, note: string) => void;
+    updatePrice: (receiptId: ReceiptId, expenseId: ExpenseId, price: number) => void;
+}
+
+const Receipt = ({ id, category, expenses, addExpense, updateCategory, updateNote, updatePrice }: ReceiptProps): JSX.Element => 
     <Box border margin='small' pad='small'>
         <Box direction='row' gap='small'>
-            <Categories />
-            <AddButton label='Add Expense' onClick={(): void => {}} />
+            <Categories receiptId={id} category={category} updateCategory={updateCategory} />
+            <AddButton label='Add Expense' onClick={(): void => addExpense(id)} />
         </Box>
+        <ExpenseList
+            receiptId={id}
+            expenses={expenses}
+            updateNote={updateNote}
+            updatePrice={updatePrice}
+        />
         <Box align='end'>
             <Total value={123} />
         </Box>
     </Box>
 
-export default Receipt
+export default observer(Receipt)
